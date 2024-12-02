eafd0269955d97f08d6efbf60a466aa9beb3b088
Skeyling1 <skeyling1@gmail.com>
2024-12-02 21:54:54 +0300

    hhh

--- WlmrCoder/main/urls.py
+++ WlmrCoder/main/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('data', views.data),
+    path('test', views.test)
 ]
+
+#Создайте новое Django приложение, зарегистрируйте его и
+# сделайте так чтоб при переходе на страницу data и test открывались страницы с разным содержимым.
--- WlmrCoder/main/views.py
+++ WlmrCoder/main/views.py
@@ -4,3 +4,20 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+
+def data(request):
+    return HttpResponse("<h1>Data</h1>"
+                        "<h1>Data</h1>"
+                        "<h1>Data</h1>"
+                        "<h1>Data</h1>"
+                        "<h1>Data</h1>"
+                        "<h1>Data</h1>"
+                        "<h1>Data</h1>")
+
+def test(request):
+    return HttpResponse("<h1>Test</h1>"
+                        "<h1>Test</h1>"
+                        "<h1>Test</h1>"
+                        "<h1>Test</h1>"
+                        "<h1>Test</h1>"
+                        "<h1>Test</h1>")
