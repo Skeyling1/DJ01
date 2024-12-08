2056bb8d27655fb9b605253e5225b1551c32f021
Skeyling1 <skeyling1@gmail.com>
2024-12-08 15:34:40 +0300

    rrr

--- WlmrCoder/news/models.py
+++ WlmrCoder/news/models.py
@@ -6,7 +6,7 @@ class News_post(models.Model):
     short_description = models.CharField('Краткое описание новости', max_length=200)
     text = models.TextField('Новость')
     pub_date = models.DateTimeField('Дата публикации')
-    author = models.CharField('Автор: ', max_length=50)
+    author = models.CharField('Автор', max_length=50)
 
 
     def __str__(self):
