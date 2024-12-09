0e76392f2eee0b37880366b01d04bff8dff9f39e
Skeyling1 <skeyling1@gmail.com>
2024-12-09 22:54:25 +0300

    jjj

--- WlmrCoder/news/urls.py
+++ WlmrCoder/news/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="news_home"),
-
+    path('create_news/', views.add_news, name="news_addition")
 ]
--- WlmrCoder/news/views.py
+++ WlmrCoder/news/views.py
@@ -7,3 +7,9 @@ def home(request):
     news = News_post.objects.all()
     data = {'caption': "Django", 'news': news}
     return render(request, 'news/news.html', data)
+
+def add_news(request):
+
+    data = {'caption': "Django"}
+    return render(request, 'news/news_add.html', data)
+
